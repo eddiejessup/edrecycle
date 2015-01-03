03343bacf10d96d3bedc6fbda64aefd270fbacc5
Elliot Marsden <elliot.marsden@gmail.com>
2015-01-03 14:48:41 +0000

    Rewrite search javascript to try to work on mobile

--- static/filter.js
+++ static/filter.js
@@ -12,36 +12,38 @@ var main = function() {
         });
     })
 
-    $('#loc').keyup(function() {
-        if ($('#loc').val().length > 3) {
-            $.getJSON($SCRIPT_ROOT + '/_lookup', {loc: $('#loc').val()}, function(data) {
-                $('.results').hide();
-                if (data.success) {
-                    $('#locname').text(data.results.location);
-                    $('#day').text(data.results.day);
-                    $('#ical').attr('href', $SCRIPT_ROOT + '/static/icals/' + data.results.filename + '.ical');
-                    $('#pdf').attr('href', data.results.pdf);
-                    if (data.results.blue) {
-                        $('#yesblue').show()
-                        $('#noblue').hide()
-                        $('#blue-date').text(data.results.next_blue_date);
-                    } else {
-                        $('#yesblue').hide()
-                        $('#noblue').show()
-                    }
-                    if (data.results.red) {
-                        $('#yesred').show()
-                        $('#nored').hide()
-                        $('#red-date').text(data.results.next_red_date);
-                    } else {
-                        $('#yesred').hide()
-                        $('#nored').show()
-                    }
-                    $('.results').show();
+}
+
+var search = function() {
+    if ($('#loc').val().length > 3) {
+        $.getJSON($SCRIPT_ROOT + '/_lookup', {loc: $('#loc').val()}, function(data) {
+            $('.results').hide();
+            if (data.success) {
+                $('#locname').text(data.results.location);
+                $('#day').text(data.results.day);
+                $('#ical').attr('href', $SCRIPT_ROOT + '/static/icals/' + data.results.filename + '.ical');
+                $('#pdf').attr('href', data.results.pdf);
+                if (data.results.blue) {
+                    $('#yesblue').show()
+                    $('#noblue').hide()
+                    $('#blue-date').text(data.results.next_blue_date);
+                } else {
+                    $('#yesblue').hide()
+                    $('#noblue').show()
                 }
-            });
-        }
-    });
+                if (data.results.red) {
+                    $('#yesred').show()
+                    $('#nored').hide()
+                    $('#red-date').text(data.results.next_red_date);
+                } else {
+                    $('#yesred').hide()
+                    $('#nored').show()
+                }
+                $('.results').show();
+            }
+        });
+    }
 }
 
 $(document).ready(main)
+$(document).on('keyup','#loc', search);
